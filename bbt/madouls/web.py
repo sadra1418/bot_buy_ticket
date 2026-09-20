bb70697d69d8605ac13a4780cdb90b992d11f9ee
Mohammad Sadra Vaziri <javadvaziri@Sadras-MBP.bbrouter>
2026-09-20 21:52:35 +0330

    backend is finished

--- bbt/madouls/web.py
+++ bbt/madouls/web.py
@@ -2,17 +2,45 @@ from playwright.async_api import async_playwright
 
 
 class Web ():
+    """
+    the browser functions 
+    """
 
 
-    async def __init__(self):
+    def __init__(self):
+        """the browser functions """
+        pass
+
+
+    async def start(self):
         self.pl = await async_playwright().start()
         self.browser = await self.pl.chromium.launch(headless=False)
         self.contex = await self.browser.new_context()
 
+
+
+#._______________micro-functions_______________#       
+
+    def time_to_num(self ,time):
+            """input = 22:10. output=2210"""
+    
+            l_t = []
+            for i in time:
+                if i != ':' and i != ',':
+                    l_t.append(i)
+            
+            return int(''.join(l_t))
+
     async def insert_text_of_person(self ,text ,selector ,tab):
                 
-                await tab.click(selector)
-                await tab.keyboard.type(text) 
+        await tab.click(selector)
+        await tab.keyboard.type(text) 
+
+    async def list_of_locator(self ,tab ,selector):
+        locator_element = tab.locator(selector)
+        return(await locator_element.all())
+
+#._______________micro-functions_______________#   
 
 
     async def main_page_desin(self ,data_of_train):
@@ -28,5 +56,75 @@ class Web ():
         await self.insert_text_of_person('eNt?,GTDVS77','[type="password"]', tab=tab)
         await tab.click('[type="submit"]')
 
+        return (tab)
 
-    
+
+#  ________________select ticket ________________ #
+
+
+
+    async def list_ticket(self ,tab ,list_of_selectors:list):
+        all_train_list = []
+        list_of_list_selectors = []
+        number_of_ticket = len(await self.list_of_locator(tab ,list_of_selectors[0]))
+
+        for selector in list_of_selectors :list_of_list_selectors.append(await self.list_of_locator(tab ,selector)) 
+        for number in range(number_of_ticket):
+            train = {
+                        'id':number,
+                        'train':list_of_list_selectors[0][number],
+                        'price':list_of_list_selectors[1][number],
+                        'times_of_start':list_of_list_selectors[2][number*2],
+                        'type_of_train':list_of_list_selectors[3][number],
+                        'button':list_of_list_selectors[4][number]
+                    }
+            all_train_list.append(train)
+        return (all_train_list)
+
+
+
+
+    async def select_ticket(self ,tab ,all_train_list ,data_of_train):
+
+        
+                        
+        for ticket in all_train_list :
+
+            times_of_start_train = self.time_to_num(await ticket['times_of_start'].inner_text())
+            price_of_train = self.time_to_num(await ticket['price'].inner_text())
+
+
+            if data_of_train['the_max_time'] >= times_of_start_train >= data_of_train['the_min_time'] and data_of_train['the_max_price'] >= price_of_train :
+                if data_of_train['buss'] :
+                    await ticket['button'].click()
+                    select = True
+                    break
+            
+                else :
+                    b_t_f = await ticket['type_of_train'].inner_text()
+                    t_t_b = b_t_f.split()
+            
+                    if 'اتوبوسي' in t_t_b:
+                        print(f'oh this buss')
+                        select = False
+            
+                    else:
+                        await ticket['button'].click()
+                        select = True
+                        break
+                                        
+            else:
+                print(f'oh this ticket is bad price={price_of_train}. timeof start thise train = {times_of_start_train}')
+                select = False
+
+        return(select)
+
+
+
+
+#  ________________select ticket ________________ #   
+
+
+
+
+
---
+++ bbt/user_app/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class database(models.Model):
+
+    
+    count_of_servers_closed = models.IntegerField(null=True)
+    
+    
